36c01ab81b5da1cc3664578924bad42d20c125e8
Adrian <72496013+adrianmrt@users.noreply.github.com>
2021-11-23 19:29:05 +0100

    updated project

--- Scaffold15/app/src/main/java/dadm/scaffold/ScaffoldActivity.java
+++ Scaffold15/app/src/main/java/dadm/scaffold/ScaffoldActivity.java
@@ -2,7 +2,7 @@ package dadm.scaffold;
 
 import android.media.AudioManager;
 import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
