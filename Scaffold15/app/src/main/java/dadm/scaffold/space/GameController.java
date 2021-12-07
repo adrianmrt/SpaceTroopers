dd5d40e650d2caedce061e54f6bfdb878896567b
Mr.T <72496013+adrianmrt@users.noreply.github.com>
2021-12-07 11:53:31 +0100

    Update GameController.java
    fixed strange bug

--- Scaffold15/app/src/main/java/dadm/scaffold/space/GameController.java
+++ Scaffold15/app/src/main/java/dadm/scaffold/space/GameController.java
@@ -18,6 +18,11 @@ public class GameController extends GameObject {
     private static final int TIME_BETWEEN_FIREUPGRADES = 500;
     private static final int TIME_BETWEEN_HEALTHUPGRADES = 500;
 
+    private int nAsteroids = 10;
+    private int nEnemies = 3;
+    private int nUpgradesH = 5;
+    private int nUpgradesF = 5;
+
     private long currentMillis;
     private List<DestroyableItem> asteroidPool = new ArrayList<DestroyableItem>();
     private List<Enemy> enemyPool = new ArrayList<Enemy>();
@@ -29,36 +34,40 @@ public class GameController extends GameObject {
     private int upgradesFireSpawned;
     private int upgradesHealthSpawned;
 
+    GameEngine gameEngine;
+
     public GameController(GameEngine gameEngine) {
         // We initialize the pool of items now
-        for (int i=0; i<10; i++) {
+        for (int i = 0; i < nAsteroids; i++) {
             asteroidPool.add(new Asteroid(this, gameEngine, R.drawable.uk_spitfire));
         }
-        for (int i=0; i<3; i++) {
+        for (int i = 0; i < nEnemies; i++) {
             enemyPool.add(new Enemy(this, gameEngine, R.drawable.uk_lancaster));
         }
-        for (int i=0; i<5; i++) {
+        for (int i = 0; i < nUpgradesF; i++) {
             upgradeFiresPool.add(new UpgradeFire(this, gameEngine, R.drawable.ammo_icon));
         }
-        for (int i=0; i<5; i++) {
+        for (int i = 0; i < nUpgradesH; i++) {
             upgradeHealthPool.add(new UpgradeHealth(this, gameEngine, R.drawable.health_icon));
         }
+
+        this.gameEngine = gameEngine;
     }
 
     @Override
     public void startGame() {
         currentMillis = 0;
         enemiesSpawned = 0;
-        asteroidsSpawned=0;
-        upgradesFireSpawned=0;
-        upgradesHealthSpawned=0;
+        asteroidsSpawned = 0;
+        upgradesFireSpawned = 0;
+        upgradesHealthSpawned = 0;
     }
 
     @Override
     public void onUpdate(long elapsedMillis, GameEngine gameEngine) {
         currentMillis += elapsedMillis;
-
-        long waveTimestampEnemies = enemiesSpawned*TIME_BETWEEN_ENEMIES;
+        refreshPools();
+        long waveTimestampEnemies = enemiesSpawned * TIME_BETWEEN_ENEMIES;
         if (currentMillis > waveTimestampEnemies && !enemyPool.isEmpty()) {
             // Spawn a new enemy
             DestroyableItem a = enemyPool.remove(0);
@@ -66,7 +75,7 @@ public class GameController extends GameObject {
             gameEngine.addGameObject(a);
             enemiesSpawned++;
         }
-        long waveTimestampAsteroids = asteroidsSpawned*TIME_BETWEEN_ASTEROIDS;
+        long waveTimestampAsteroids = asteroidsSpawned * TIME_BETWEEN_ASTEROIDS;
         if (currentMillis > waveTimestampAsteroids) {
             // Spawn a new asteroid
             DestroyableItem a = asteroidPool.remove(0);
@@ -75,8 +84,8 @@ public class GameController extends GameObject {
             asteroidsSpawned++;
         }
 
-        long waveTimestampUpgradesFire = upgradesFireSpawned* TIME_BETWEEN_FIREUPGRADES;
-        if (currentMillis > waveTimestampUpgradesFire&&!upgradeFiresPool.isEmpty()) {
+        long waveTimestampUpgradesFire = upgradesFireSpawned * TIME_BETWEEN_FIREUPGRADES;
+        if (currentMillis > waveTimestampUpgradesFire && !upgradeFiresPool.isEmpty()) {
             // Spawn a new upgrade
             UpgradeFire a = upgradeFiresPool.remove(0);
             a.init(gameEngine);
@@ -84,8 +93,8 @@ public class GameController extends GameObject {
             upgradesFireSpawned++;
         }
 
-        long waveTimestampUpgradesHealth = upgradesHealthSpawned* TIME_BETWEEN_HEALTHUPGRADES;
-        if (currentMillis > waveTimestampUpgradesHealth&&!upgradeHealthPool.isEmpty()) {
+        long waveTimestampUpgradesHealth = upgradesHealthSpawned * TIME_BETWEEN_HEALTHUPGRADES;
+        if (currentMillis > waveTimestampUpgradesHealth && !upgradeHealthPool.isEmpty()) {
             // Spawn a new upgrade
             UpgradeHealth a = upgradeHealthPool.remove(0);
             a.init(gameEngine);
@@ -102,10 +111,35 @@ public class GameController extends GameObject {
     public void returnToPool(DestroyableItem asteroid) {
         asteroidPool.add(asteroid);
     }
+
     public void returnToPool(Enemy enemy) {
         enemyPool.add(enemy);
     }
+
     public void returnToPool(UpgradeFire upgradeFire) {
         upgradeFiresPool.add(upgradeFire);
+    }
+
+    public void refreshPools() {
+        if (asteroidPool.size() == nAsteroids / 2) {
+            for (int i = 0; i < nAsteroids / 2; i++) {
+                asteroidPool.add(new Asteroid(this, gameEngine, R.drawable.uk_spitfire));
+            }
+        }
+        if (enemyPool.size() == nEnemies / 2) {
+            for (int i = 0; i < nEnemies / 2; i++) {
+                enemyPool.add(new Enemy(this, gameEngine, R.drawable.uk_lancaster));
+            }
+        }
+        if (upgradeFiresPool.size() == nUpgradesF / 2) {
+            for (int i = 0; i < nAsteroids / 2; i++) {
+                upgradeFiresPool.add(new UpgradeFire(this, gameEngine, R.drawable.ammo_icon));
+            }
+        }
+        if (upgradeHealthPool.size() == nUpgradesH / 2) {
+            for (int i = 0; i < nUpgradesH / 2; i++) {
+                upgradeHealthPool.add(new UpgradeHealth(this, gameEngine, R.drawable.health_icon));
+            }
+        }
     }
 }
